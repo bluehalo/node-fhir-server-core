e6f7e09b9ae91df996a70210782ec365c51d66d7
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-12-01 15:32:05 -0500

    Added comments about temporary validator

--- src/server/patient/routes/patient.routes.js
+++ src/server/patient/routes/patient.routes.js
@@ -2,6 +2,11 @@ const path = require('path');
 const controller = require(path.resolve('./src/server/patient/controllers/patient.controller'));
 const errors = require(path.resolve('./src/server/utils/error.utils'));
 
+/**
+ * This is an example validator, it will not be updated for some better
+ * middleware in the near future. It is just showing how to use the
+ * error.utils module, e.g. by calling next(errors.invalidParameter());
+ */
 let validator = (req, res, next) => {
   let sanitized = {};
   
