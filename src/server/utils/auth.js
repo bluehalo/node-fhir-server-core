f7c8893e94c1c0ab4fa094fd6ef0c5fd9a658177
Jon Lee <jlee@asymmetrik.com>
2017-12-05 11:15:22 -0500

    parse bearer token from authorization header

--- src/server/utils/auth.js
+++ src/server/utils/auth.js
@@ -7,19 +7,52 @@ const logger = require(path.resolve('./src/lib/winston'));
 
 
 /**
+ * Parse the `token` from the given
+ * `req`'s authorization header.
+ *
+ * @api public
+ * @param {Request} req
+ * @return {String|null}
+ */
+function _parseBearerToken(req) {
+
+    let auth;
+    if (!req.headers || !(auth = req.headers.authorization)) {
+      return null;
+    }
+
+    // split on space
+    const parts = auth.split(' ');
+    if (parts.length < 2) {
+        return;
+    }
+
+    // get schema and token from array
+    const schema = parts.shift().toLowerCase();
+    const token = parts.join(' ');
+
+    // validate that it is a bearer
+    if (schema !== 'bearer') {
+        return null;
+    }
+
+    return token;
+  }
+
+/**
  * Verify the JWT token
  *
  * @param {*} token
  * @param {*} secretOrPublicKey
  * @param {*} next
  */
-function _verifyToken(token, secretOrPublicKey, next) {
-
+function _verifyToken(token, secretOrPublicKey, options = {}, next) {
     const issuer = config.authConfig.issuer;
     const clientId = config.clientId;
+    const allOptions = Object.assign(options, { audience: clientId, issuer: issuer });
 
     // verify the token and signature with secret/pub key
-    jwt.verify(token, secretOrPublicKey, { audience: clientId, issuer: issuer }, function(err, decoded) {
+    jwt.verify(token, secretOrPublicKey, allOptions, function(err, decoded) {
         if (err) {
             // log error return 401 with error message;
             logger.error(err, err.message);
@@ -39,22 +72,22 @@ function _verifyToken(token, secretOrPublicKey, next) {
  */
 module.exports.validate = (req, res, next) => {
     // get bearer token
-    if (req.headers && req.headers.bearer) {
-        // testing for cors - get from params but needs to be in header
-        const bearerToken = req.headers.bearer;
-        const decodedToken = jwt.decode(bearerToken);
+    const bearerToken = _parseBearerToken(req);
+
+    if (bearerToken) {
+        const decodedToken = jwt.decode(bearerToken, {complete: true});
 
         // check algorithm so we know how to validate the signature
         if (decodedToken && decodedToken.header && decodedToken.header.alg) {
             if (decodedToken.header.alg.startsWith('HS')) {
                 // IF HS*** algorith, validate signature based on secret key
-                _verifyToken(bearerToken, config.secret, next);
+                _verifyToken(bearerToken, config.secret, {}, next);
 
             } else if (decodedToken.header.alg.startsWith('RS')) {
                 // IF RS*** algorithm, validate signature based on certificate
-                // Get public key
-                const cert = fs.readFileSync(config.security.key);
-                _verifyToken(bearerToken, cert, next);
+                // Get public key (update this to point to the correct public key path)
+                const cert = fs.readFileSync(config.security.cert);
+                _verifyToken(bearerToken, cert, {algorithms: [decodedToken.header.alg]}, next);
             }
         }
     }
