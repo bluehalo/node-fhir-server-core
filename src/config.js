65cde098571f84531210723e263d4810c60ef3a6
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-12-07 11:29:25 -0500

    Added some github markdown files Added more config options

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@
  * @description Export default configurations for the FHIR server
  */
 module.exports = {
+	swaggerHelperDefinitions: 'src/server/*/definitions/*.json',
+	swaggerRootDefinitions: 'src/server/*/*.swagger.json',
 	conformanceStatements: 'src/server/*/conformance.js',
 	routes: 'src/server/**/*.routes.js'
 };
