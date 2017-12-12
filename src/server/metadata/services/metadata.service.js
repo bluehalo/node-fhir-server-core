2c46c2621b6e1abac38a80feb5647643def6fb03
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-12-12 09:32:36 -0500

    Updated names to clean up generating capability statement

--- src/server/metadata/services/metadata.service.js
+++ src/server/metadata/services/metadata.service.js
@@ -4,17 +4,23 @@ const { files } = require(path.resolve('./src/config'));
 const { makeStatement } = require(path.resolve('./src/server/metadata/capability'));
 
 // Load all the conformance documents ahead of time
-const CONFORMANCE_DOCS = glob
+const RESOURCES = glob
 	.sync(files.conformanceStatements)
-	.map(document_path => ({ document_path, statement: require(path.resolve(document_path)) }));
+	.map(resource_path => {
+		// Resource is a function that returns the conformance statement for this resource
+		// and takes the number of that particular resource, (conformance statements need a count)
+		const { Profile, Resource } = require(path.resolve(resource_path));
+		return { Profile, Resource };
+	});
 
-// Create some objects based on the available profiles
-let mapConformanceDocuments = profiles => {
-	return ({ document_path, statement }) => {
-		const profile_name = Object.keys(profiles).find(name => document_path.indexOf(name) > -1);
+// Create some objects with some data from the available profiles
+let mapResources = profiles => {
+	return ({ Profile, Resource }) => {
+		// If we do not find a profile_name in the profiles, we will not be able to
+		// getCount and then this resource will be filtered out in the next iteration
+		const profile_name = Object.keys(profiles).find(name => name === Profile);
 		return {
-			name: profile_name,
-			conformanceFunc: statement,
+			makeResource: Resource,
 			getCount: profiles[profile_name]
 				&& profiles[profile_name].serviceModule
 				&& profiles[profile_name].serviceModule.getCount
@@ -22,8 +28,10 @@ let mapConformanceDocuments = profiles => {
 	};
 };
 
-// Remove any documents that did not find a matching name and getCount method from the available profiles
-let filterConformanceDocuments = (doc) => doc.name && doc.conformanceFunc && doc.getCount;
+// Remove any resources that did not find a getCount method from the available profiles
+// Each conformance statement MUST have a count, if there is no way to retrieve it then
+// do not show it
+let filterResources = (resource) => resource.makeResource && resource.getCount;
 
 /**
  * @function generateCapabilityStatement
@@ -36,25 +44,25 @@ let generateCapabilityStatement = (req, profiles, logger) => new Promise((resolv
 	logger.info('Metadata.generateCapabilityStatement');
 	// Create a new base capability statement per request
 
-	// Iterate over the availalbe conformance docs
-	let conformance_documents = CONFORMANCE_DOCS
+	// Iterate over the availalbe resources
+	let active_resources = RESOURCES
 		// generate some useful objects, these will be used to make the statement
 		// and query for the count of available records for the specific profile
-		.map(mapConformanceDocuments(profiles))
+		.map(mapResources(profiles))
 		// Remove any ones that we do not have, we do not want to show conformance
 		// statements for profiles that we do not support
-		.filter(filterConformanceDocuments);
+		.filter(filterResources);
 
 	// Iterate over all the filtered docs and make queries for each one
 	// based on the getCount service method, req and logger are by no means necessary
 	// but pass them in so the service can log in the same manner and see request if necessary
 	// for any validation etc,
-	return Promise.all(conformance_documents.map(doc => doc.getCount(req, logger)))
+	return Promise.all(active_resources.map(resource => resource.getCount(req, logger)))
 		.then((results) => {
-			// Generate the completed statement, conformanceFunc just adds the count to the json
-			let resources = conformance_documents.map((doc, i) => doc.conformanceFunc(results[i]));
+			// Generate the resources conformance statement and add these to the main Capability Statement
+			let resource_statements = active_resources.map((resource, i) => resource.makeResource(results[i]));
 			// Add these resources to the main CapabilityStatement
-			return resolve(makeStatement(resources));
+			return resolve(makeStatement(resource_statements));
 		})
 		.catch(reject);
 });
--- src/server/patient/conformance.js
+++ src/server/patient/conformance.js
@@ -3,48 +3,51 @@
  * @summary Patient conformance statement
  */
 
-module.exports = (count) => ({
-	extension: [{
-		url: 'http://hl7api.sourceforge.net/hapi-fhir/res/extdefs.html#resourceCount',
-		// This will be resolved dynamically by the service methods
-		valueDecimal: count
-	}],
-	type: 'Patient',
-	profile: {
-		reference: 'http://hl7.org/fhir/Profile/Patient'
-	},
-	interaction: [{
-		code: 'read'
-	}, {
-		code: 'vread'
-	}],
-	searchParam: [{
-		name: 'id',
-		type: 'token',
-		definition: 'Id of the patient'
-	}, {
-		name: 'name',
-		type: 'string',
-		definition: 'Name of the patient'
-	}, {
-		name: 'birthdate',
-		type: 'date',
-		definition: 'Birthdate of the patient'
-	}, {
-		name: 'gender',
-		type: 'token',
-		definition: 'Gender of the patient'
-	}, {
-		name: 'given',
-		type: 'string',
-		definition: 'A portion of the given name of the patient'
-	}, {
-		name: 'family',
-		type: 'string',
-		definition: 'A portion of the family name of the patient'
-	}, {
-		name: 'identifier',
-		type: 'token',
-		definition: 'A patient identifier'
-	}]
-});
+module.exports = {
+	Profile: 'patient',
+	Resource: (count) => ({
+		extension: [{
+			url: 'http://hl7api.sourceforge.net/hapi-fhir/res/extdefs.html#resourceCount',
+			// This will be resolved dynamically by the service methods
+			valueDecimal: count
+		}],
+		type: 'Patient',
+		profile: {
+			reference: 'http://hl7.org/fhir/Profile/Patient'
+		},
+		interaction: [{
+			code: 'read'
+		}, {
+			code: 'vread'
+		}],
+		searchParam: [{
+			name: 'id',
+			type: 'token',
+			definition: 'Id of the patient'
+		}, {
+			name: 'name',
+			type: 'string',
+			definition: 'Name of the patient'
+		}, {
+			name: 'birthdate',
+			type: 'date',
+			definition: 'Birthdate of the patient'
+		}, {
+			name: 'gender',
+			type: 'token',
+			definition: 'Gender of the patient'
+		}, {
+			name: 'given',
+			type: 'string',
+			definition: 'A portion of the given name of the patient'
+		}, {
+			name: 'family',
+			type: 'string',
+			definition: 'A portion of the family name of the patient'
+		}, {
+			name: 'identifier',
+			type: 'token',
+			definition: 'A patient identifier'
+		}]
+	})
+};
