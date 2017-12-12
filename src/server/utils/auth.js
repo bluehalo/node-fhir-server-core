423a8f207788bfec06fccf332f20b6ab7b791505
Eddie Davis <edavis@asymmetrik.com>
2017-12-12 09:43:24 -0500

    Removed unused files, fixed scope logic if in token, reformatted.

--- src/server/utils/auth.js
+++ src/server/utils/auth.js
@@ -16,38 +16,39 @@ let logger;
  * @returns {Boolean}
  */
 function _hasCorrectScope(decodedTokenOrIntrospection, validScopes) {
-    const tokenScope = decodedTokenOrIntrospection.scope;
-    const scopeArray = typeof tokenScope === 'string' ? tokenScope.split(' ') : [];
-    const intersection = _.intersection(scopeArray, validScopes);
+	const tokenScope = decodedTokenOrIntrospection.scope;
+	const scopeArray = typeof tokenScope === 'string' ? tokenScope.split(' ') : [];
+	const intersection = _.intersection(scopeArray, validScopes);
 
-    logger.debug(`Required scopes for resource ${validScopes}`);
-    logger.debug(`Scope found on request ${scopeArray}`);
+	logger.debug(`Required scopes for resource ${validScopes}`);
+	logger.debug(`Scope found on request ${scopeArray}`);
 
-    return intersection.length > 0;
+	return intersection.length > 0;
 }
 
 /**
  * Returns the appropriate key to verify the JWT.
  *
  * @param {Object} decodedToken
+ * @param {Object} oauthConfig
  * @returns {String}
  */
 function _getKeyForTokenValidation(decodedToken, oauthConfig) {
 
-		// Check for a private key first
-    if (oauthConfig.secretKey) {
-				return oauthConfig.secretKey;
+	// Check for a private key first
+	if (oauthConfig.secretKey) {
+		return oauthConfig.secretKey;
 		// Then check if there is a key in the jwkSet.
-    } else if (_.get(oauthConfig, 'jwkSet.keys', []).length === 1) {
-        const jwtKey = oauthConfig.jwkSet.keys[0];
-				return jwkToPem(jwtKey);
+	} else if (_.get(oauthConfig, 'jwkSet.keys', []).length === 1) {
+		const jwtKey = oauthConfig.jwkSet.keys[0];
+		return jwkToPem(jwtKey);
 		// If there is more than one key, check the key identifier.
-    } else if (_.get(oauthConfig, 'jwkSet.keys', []).length > 1) {
-        const jwtKey = _.find(oauthConfig.jwkSet.keys, ['kid', decodedToken.header.kid]);
-        return jwkToPem(jwtKey);
-    } else {
-        return null;
-    }
+	} else if (_.get(oauthConfig, 'jwkSet.keys', []).length > 1) {
+		const jwtKey = _.find(oauthConfig.jwkSet.keys, ['kid', decodedToken.header.kid]);
+		return jwkToPem(jwtKey);
+	} else {
+		return null;
+	}
 }
 
 /**
@@ -58,27 +59,27 @@ function _getKeyForTokenValidation(decodedToken, oauthConfig) {
  */
 function _parseBearerToken(req) {
 
-    let auth;
-    if (!req.headers || !(auth = req.headers.authorization)) {
-				return null;
-    }
+	let auth;
+	if (!req.headers || !(auth = req.headers.authorization)) {
+		return null;
+	}
 
-    // split on space
-    const parts = auth.split(' ');
-    if (parts.length < 2) {
-        return null;
-    }
+	// split on space
+	const parts = auth.split(' ');
+	if (parts.length < 2) {
+		return null;
+	}
 
-    // get schema and token from array
-    const schema = parts.shift().toLowerCase();
-    const token = parts.join(' ');
+	// get schema and token from array
+	const schema = parts.shift().toLowerCase();
+	const token = parts.join(' ');
 
-    // validate that it is a bearer
-    if (schema !== 'bearer') {
-        return null;
-    }
+	// validate that it is a bearer
+	if (schema !== 'bearer') {
+		return null;
+	}
 
-    return token;
+	return token;
 }
 
 /**
@@ -88,83 +89,86 @@ function _parseBearerToken(req) {
  * @param {String} secretOrPublicKey
  * @param {Object} options
  * @param {Array<String>} validScopes
+ * @param {Object} oauthConfig
  * @param {Function} next
  * @returns {Promise} Returns a promise that resolves to the result of the `next`.
  */
 async function _verifyToken(token, secretOrPublicKey, options = {}, validScopes, oauthConfig, next) {
-    const issuer = oauthConfig.issuer;
-    const clientId = oauthConfig.clientId;
-    const allOptions = Object.assign(options, { audience: clientId, issuer: issuer });
+	const issuer = oauthConfig.issuer;
+	const clientId = oauthConfig.clientId;
+	const allOptions = Object.assign(options, { audience: clientId, issuer: issuer });
 
-		// verify the token and signature with secret/pub key
-		let decoded;
-    try {
-				decoded = jwt.verify(token, secretOrPublicKey, allOptions);
-		} catch (err) {
-        // log error return 401 with error message;
-        logger.error(err, err.message);
-        return next(errors.custom(401, 'invalid_token'));
-		}
+	// verify the token and signature with secret/pub key
+	let decoded;
+	try {
+		decoded = jwt.verify(token, secretOrPublicKey, allOptions);
+	} catch (err) {
+		// log error return 401 with error message;
+		logger.error(err, err.message);
+		return next(errors.custom(401, 'invalid_token'));
+	}
 
-		logger.debug('Token has been verified. Searching for scope ...');
+	logger.debug('Token has been verified. Searching for scope ...');
 
-		if (typeof decoded.scope === 'string' && _hasCorrectScope(decoded, validScopes)) {
-				return next();
-		} else if (oauthConfig.introspection_endpoint) {
-				logger.debug('Attempting to introspect token');
+	if (typeof decoded.scope === 'string') {
+		return _hasCorrectScope(decoded, validScopes) ? next() : next(errors.custom(403, 'insufficient_scope'));
+	} else if (oauthConfig.introspection_endpoint) {
+		logger.debug('Attempting to introspect token');
 
-				const protectedResourceClientId = oauthConfig.protectedResourceClientId;
-				const protectedResourceClientSecret = oauthConfig.protectedResourceClientSecret;
+		const protectedResourceClientId = oauthConfig.protectedResourceClientId;
+		const protectedResourceClientSecret = oauthConfig.protectedResourceClientSecret;
 
-				try {
-						const introspectionResponse = await request
-								.post(oauthConfig.introspection_endpoint)
-								.auth(protectedResourceClientId, protectedResourceClientSecret)
-								.send(`token=${token}`);
-						const introspection = introspectionResponse.body;
+		try {
+			const introspectionResponse = await request
+				.post(oauthConfig.introspection_endpoint)
+				.auth(protectedResourceClientId, protectedResourceClientSecret)
+				.send(`token=${token}`);
+			const introspection = introspectionResponse.body;
 
-						logger.debug('Successfully introspected token');
-						if (!introspection.active) {
-								logger.error('Access token is not active');
-								return next(errors.custom(401, 'invalid_token'));
-						} else if (!_hasCorrectScope(introspection, validScopes)) {
-								logger.error('Access token has insufficient scope');
-								return next(errors.custom(403, 'insufficient_scope'));
-						} else {
-								logger.info('Access token and scope have been verified');
-								return next();
-						}
-				} catch (error) {
-					logger.error(`Failed to introspect token ${error}`);
-					return next(errors.custom(403, 'insufficient_scope'));
-				}
-		} else {
-				logger.error('The scope of the token is insufficient or the token cannot be introspected');
+			logger.debug('Successfully introspected token');
+			if (!introspection.active) {
+				logger.error('Access token is not active');
+				return next(errors.custom(401, 'invalid_token'));
+			} else if (!_hasCorrectScope(introspection, validScopes)) {
+				logger.error('Access token has insufficient scope');
 				return next(errors.custom(403, 'insufficient_scope'));
+			} else {
+				logger.info('Access token and scope have been verified');
+				return next();
+			}
+		} catch (error) {
+			logger.error(`Failed to introspect token ${error}`);
+			return next(errors.custom(403, 'insufficient_scope'));
 		}
+	} else {
+		logger.error('The scope of the token is insufficient or the token cannot be introspected');
+		return next(errors.custom(403, 'insufficient_scope'));
+	}
 }
 
 /**
  * @name validate
  * @summary {Function} Returns a middleware function that verifies bearer token and checks scope
  * @param {Array<String>} validScopes A list of scopes that allow access to the resource.
+ * @param {Object} loggerUtil
+ * @param {Object} oauthConfig
  */
 module.exports.validate = (validScopes, loggerUtil, oauthConfig) => {
-		logger = logger || loggerUtil;
-    return async (req, res, next) => {
-        // get bearer token
-        const bearerToken = _parseBearerToken(req);
+	logger = logger || loggerUtil;
+	return async (req, res, next) => {
+		// get bearer token
+		const bearerToken = _parseBearerToken(req);
 
-        if (bearerToken) {
-            logger.debug('Found bearer token in request');
-            const decodedToken = jwt.decode(bearerToken, {complete: true});
-            logger.debug('Decoded bearer token from request');
-            const tokenKey = _getKeyForTokenValidation(decodedToken, oauthConfig);
-            return await _verifyToken(bearerToken, tokenKey, {}, validScopes, oauthConfig, next);
-        } else {
-            // did not pass checks, return 401 message
-            logger.error('Could not find bearer token in request headers');
-            return next(errors.unauthorized());
-        }
-    };
+		if (bearerToken) {
+			logger.debug('Found bearer token in request');
+			const decodedToken = jwt.decode(bearerToken, { complete: true });
+			logger.debug('Decoded bearer token from request');
+			const tokenKey = _getKeyForTokenValidation(decodedToken, oauthConfig);
+			return await _verifyToken(bearerToken, tokenKey, {}, validScopes, oauthConfig, next);
+		} else {
+			// did not pass checks, return 401 message
+			logger.error('Could not find bearer token in request headers');
+			return next(errors.unauthorized());
+		}
+	};
 };
