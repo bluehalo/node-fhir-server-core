612c1de4eb13d5e6a5425b29506a5bc95c63d968
Jon Lee <jlee@asymmetrik.com>
2018-01-02 14:27:52 -0500

    dont validate on return.

--- src/server/observation/controllers/observation.controller.js
+++ src/server/observation/controllers/observation.controller.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const { ServerError } = require(path.resolve('./src/server/utils/error.utils'));
-const Validator = require('jsonschema').Validator;
-const observationSchema = require(path.resolve('./src/server/schemas/observation.schema'));
 
 module.exports.getObservations = (profile, logger) => {
 	let { serviceModule: service } = profile;
@@ -23,29 +21,19 @@ module.exports.getObservations = (profile, logger) => {
 				};
 
 				if (observations) {
-					const v = new Validator();
 					for (let resource of observations) {
-
-						// serialize
-						const serialized = JSON.parse(JSON.stringify(resource));
-
-						// validate all resources
-						if (v.validate(serialized, observationSchema).valid) {
-							// Modes:
-							// match - This resource matched the search specification.
-							// include - This resource is returned because it is referred to from another resource in the search set.
-							// outcome - An OperationOutcome that provides additional information about the processing of a search.
-							const entry = {
-								'search': {
-									'mode': 'match'
-								},
-								'resource': serialized,
-								'fullUrl': `localhost:3000/Observation/${resource.id}`
-							};
-							searchResults.entry.push(entry);
-						} else {
-							throw new Error('Invalid Observation: ' + resource.id);
-						}
+						// Modes:
+						// match - This resource matched the search specification.
+						// include - This resource is returned because it is referred to from another resource in the search set.
+						// outcome - An OperationOutcome that provides additional information about the processing of a search.
+						const entry = {
+							'search': {
+								'mode': 'match'
+							},
+							'resource': resource.toJSON(),
+							'fullUrl': `localhost:3000/Observation/${resource.id}`
+						};
+						searchResults.entry.push(entry);
 					}
 				}
 
@@ -69,18 +57,7 @@ module.exports.getObservationByID = (profile, logger) => {
 		*/
 		return service.getObservation(req, logger)
 			.then((observation) => {
-
-				// serialize
-				const serialized = JSON.parse(JSON.stringify(observation));
-
-				// validate the Observation Object
-				const v = new Validator();
-				if (v.validate(serialized, observationSchema).valid) {
-					res.send(serialized);
-				} else {
-					//
-					throw new Error('Invalid Observation');
-				}
+					res.send(observation.toJSON);
 			})
 			.catch((err) => {
 				next(new ServerError(500, err.message));
