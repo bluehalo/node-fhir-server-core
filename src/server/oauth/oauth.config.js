30e6689c3063af195b174bab4c6e53f7d4651811
Jon Lee <jlee@asymmetrik.com>
2018-01-03 14:15:23 -0500

    fixed spacing and comment

--- src/server/oauth/oauth.config.js
+++ src/server/oauth/oauth.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
 const controller = require(path.resolve('./src/server/oauth/controllers/oauth.controller'));
 let routes = [
-  {
-    type: 'get',
-    path: '/dstu2/authorize',
-    args: [{
-      name: 'aud',
+	{
+		type: 'get',
+		path: '/dstu2/authorize',
+		args: [{
+			name: 'aud',
 			type: 'string',
 			required: true
-    }, {
-      name: 'launch',
+		}, {
+			name: 'launch',
 			type: 'string',
 			required: true
-    }, {
-      name: 'clientId',
-      type: 'string',
+		}, {
+			name: 'clientId',
+			type: 'string',
 			required: true
-    }, {
-      name: 'scope',
-      type: 'string',
+		}, {
+			name: 'scope',
+			type: 'string',
 			required: true
-    }],
-    scopes: [],
-    controller: controller.authorize
-  },
-  {
-    type: 'post',
-    path: '/dstu2/token',
-    args: [],
-    scopes: [],
-    controller: controller.token
-  }
+		}],
+		scopes: [],
+		controller: controller.authorize
+	},
+	{
+		type: 'post',
+		path: '/dstu2/token',
+		args: [],
+		scopes: [],
+		controller: controller.token
+	}
 ];
 
 
 
 /**
  * @name exports
- * @summary Observations config
+ * @summary OAUTH config
  */
 module.exports = {
-  routes
+	routes
 };
