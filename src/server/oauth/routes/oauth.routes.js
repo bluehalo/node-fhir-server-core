6bb37a593759a20c6ef24f1b12e5d494a0e3a47b
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2018-01-05 17:22:56 -0500

    Fixed path

--- src/server/oauth/routes/oauth.routes.js
+++ src/server/oauth/routes/oauth.routes.js
@@ -1,6 +1,5 @@
-const path = require('path');
-const { sanitizeMiddleware } = require(path.resolve('./src/server/utils/sanitize.utils'));
-const { routes } = require(path.resolve('./src/server/oauth/oauth.config'));
+const { sanitizeMiddleware } = require('../../utils/sanitize.utils');
+const { routes } = require('../oauth.config');
 
 
 /**
