7d50d5ebb34033dfe94df09be860f6750b565e2e
Jon Lee <jlee@asymmetrik.com>
2018-01-11 13:57:08 -0500

    missing comma

--- src/runner.js
+++ src/runner.js
@@ -7,7 +7,7 @@ const CONFIG = {
 		discoveryUrl: 'https://sb-auth.smarthealthit.org/.well-known/openid-configuration',
 
 		protectedResourceClientId: 'ae83b0eb-35ed-483b-a933-edb2277f4aad',
-		protectedResourceClientSecret: 'AK1MPC0PT44icz7awMie4Pasd9BOMcJ6rTAazq2Ni01nQLecxqqtrcXKrz4bciQOaL5tjclmSKO064u9n1IoDzE'
+		protectedResourceClientSecret: 'AK1MPC0PT44icz7awMie4Pasd9BOMcJ6rTAazq2Ni01nQLecxqqtrcXKrz4bciQOaL5tjclmSKO064u9n1IoDzE',
 
 		// If there is no discovery url, these values must be provided.
 		/*
