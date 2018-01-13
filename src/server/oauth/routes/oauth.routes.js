b1976dc85a2ca37d524f8538a78b1562a749e90c
Jon Lee <jlee@asymmetrik.com>
2018-01-13 17:40:06 -0500

    Adding back in CORS for routes to allow OPTIONS

--- src/server/oauth/routes/oauth.routes.js
+++ src/server/oauth/routes/oauth.routes.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const { routes } = require('../oauth.config');
 
 /**
@@ -5,7 +6,7 @@ const { routes } = require('../oauth.config');
  * @summary Oauth routes
  */
 module.exports = (app, config, logger) => {
-	let { profiles } = config;
+	let { profiles, server } = config;
 
 	let oauth = profiles.dstu2 && profiles.dstu2.oauth;
 
@@ -13,7 +14,13 @@ module.exports = (app, config, logger) => {
 	// Only add routes if we have a oauth profile
 	// the endpoint can't function without the config
 	if (oauth) {
+		let baseOptions = Object.assign({}, server.corsOptions, oauth.corsOptions);
+
 		routes.forEach((route) => {
+			let corsOptions = Object.assign({}, baseOptions, route.corsOptions);
+			// Enable options
+			app.options(route.path, cors(corsOptions));
+
 			// Enable route
 			app[route.type](
 				route.path,
