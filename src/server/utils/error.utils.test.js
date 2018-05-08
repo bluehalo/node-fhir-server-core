bb5a6f3c624959279939df62a2953e5e1be4d16f
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2018-05-08 15:14:58 -0400

    Added some error tests Fixed routing for observations

---
+++ src/server/utils/error.utils.test.js
@@ -0,0 +1,135 @@
+const {
+	invalidParameter,
+	unauthorized,
+	insufficientScope,
+	notFound,
+	deleted,
+	internal,
+	isServerError
+} = require('./error.utils');
+
+const {
+	ISSUE,
+	VERSIONS
+} = require('../../constants');
+
+describe('Error Utils Tests', () => {
+
+	test('should return correct OperationOutcome for an invalidParameter error', () => {
+		let error = invalidParameter('Age is invalid.', VERSIONS.STU3);
+		let issue = error.issue[0];
+
+		expect(error.statusCode).toEqual(400);
+		expect(error.issue).toHaveLength(1);
+
+		expect(issue.code).toBe(ISSUE.CODE.INVALID);
+		expect(issue.severity).toBe(ISSUE.SEVERITY.ERROR);
+		expect(issue.diagnostics).toBe('Age is invalid.');
+	});
+
+	test('should return correct OperationOutcome for an unauthorized error', () => {
+		let errorWithMessage = unauthorized('You shall not pass.', VERSIONS.STU3);
+		let errorWithoutMessage = unauthorized('', VERSIONS.STU3);
+
+		expect(errorWithMessage.statusCode).toEqual(401);
+		expect(errorWithMessage.issue).toHaveLength(1);
+
+		let issueWithMessage = errorWithMessage.issue[0];
+		let issueWithoutMessage = errorWithoutMessage.issue[0];
+
+		// Check the code and severity on one
+		expect(issueWithMessage.code).toBe(ISSUE.CODE.FORBIDDEN);
+		expect(issueWithoutMessage.severity).toBe(ISSUE.SEVERITY.ERROR);
+
+		// Check the message for both
+		expect(issueWithMessage.diagnostics).toBe('You shall not pass.');
+		expect(issueWithoutMessage.diagnostics).toBe('401: Unauthorized request');
+	});
+
+	test('should return correct OperationOutcome for an insufficientScope error', () => {
+		let errorWithMessage = insufficientScope('Not enough scope for this action.', VERSIONS.STU3);
+		let errorWithoutMessage = insufficientScope('', VERSIONS.STU3);
+
+		expect(errorWithMessage.statusCode).toEqual(403);
+		expect(errorWithMessage.issue).toHaveLength(1);
+
+		let issueWithMessage = errorWithMessage.issue[0];
+		let issueWithoutMessage = errorWithoutMessage.issue[0];
+
+		// Check the code and severity on one
+		expect(issueWithMessage.code).toBe(ISSUE.CODE.FORBIDDEN);
+		expect(issueWithoutMessage.severity).toBe(ISSUE.SEVERITY.ERROR);
+
+		// Check the message for both
+		expect(issueWithMessage.diagnostics).toBe('Not enough scope for this action.');
+		expect(issueWithoutMessage.diagnostics).toBe('403: Insufficient scope');
+	});
+
+	test('should return correct OperationOutcome for an notFound error', () => {
+		let errorWithMessage = notFound('Somthing not found.', VERSIONS.STU3);
+		let errorWithoutMessage = notFound('', VERSIONS.STU3);
+
+		expect(errorWithMessage.statusCode).toEqual(404);
+		expect(errorWithMessage.issue).toHaveLength(1);
+
+		let issueWithMessage = errorWithMessage.issue[0];
+		let issueWithoutMessage = errorWithoutMessage.issue[0];
+
+		// Check the code and severity on one
+		expect(issueWithMessage.code).toBe(ISSUE.CODE.NOT_FOUND);
+		expect(issueWithoutMessage.severity).toBe(ISSUE.SEVERITY.ERROR);
+
+		// Check the message for both
+		expect(issueWithMessage.diagnostics).toBe('Somthing not found.');
+		expect(issueWithoutMessage.diagnostics).toBe('404: Not found');
+	});
+
+	test('should return correct OperationOutcome for an deleted error', () => {
+		let errorWithMessage = deleted('This resource has been deleted.', VERSIONS.STU3);
+		let errorWithoutMessage = deleted('', VERSIONS.STU3);
+
+		expect(errorWithMessage.statusCode).toEqual(410);
+		expect(errorWithMessage.issue).toHaveLength(1);
+
+		let issueWithMessage = errorWithMessage.issue[0];
+		let issueWithoutMessage = errorWithoutMessage.issue[0];
+
+		// Check the code and severity on one
+		expect(issueWithMessage.code).toBe(ISSUE.CODE.NOT_FOUND);
+		expect(issueWithoutMessage.severity).toBe(ISSUE.SEVERITY.ERROR);
+
+		// Check the message for both
+		expect(issueWithMessage.diagnostics).toBe('This resource has been deleted.');
+		expect(issueWithoutMessage.diagnostics).toBe('410: Resource deleted');
+	});
+
+	test('should return correct OperationOutcome for an internal error', () => {
+		let errorWithMessage = internal('Internal Server Error.', VERSIONS.STU3);
+		let errorWithoutMessage = internal('', VERSIONS.STU3);
+
+		expect(errorWithMessage.statusCode).toEqual(500);
+		expect(errorWithMessage.issue).toHaveLength(1);
+
+		let issueWithMessage = errorWithMessage.issue[0];
+		let issueWithoutMessage = errorWithoutMessage.issue[0];
+
+		// Check the code and severity on one
+		expect(issueWithMessage.code).toBe(ISSUE.CODE.EXCEPTION);
+		expect(issueWithoutMessage.severity).toBe(ISSUE.SEVERITY.ERROR);
+
+		// Check the message for both
+		expect(issueWithMessage.diagnostics).toBe('Internal Server Error.');
+		expect(issueWithoutMessage.diagnostics).toBe('500: Internal server error');
+	});
+
+	test('should correctly determine if an error is an OperationOutcome', () => {
+		let normalError = new Error('FooBar');
+		let operationError = invalidParameter('', VERSIONS.STU3);
+		let operationErrorWithoutVersion = invalidParameter('');
+
+		expect(isServerError(normalError, VERSIONS.STU3)).toBeFalsy();
+		expect(isServerError(operationError, VERSIONS.STU3)).toBeTruthy();
+		expect(isServerError(operationErrorWithoutVersion, VERSIONS.STU3)).toBeTruthy();
+	});
+
+});
