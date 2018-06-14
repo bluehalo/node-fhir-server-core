d7d65b0b227f12afbf00d381fbcb55f9296bc21d
Rt <37812310+imrts@users.noreply.github.com>
2018-06-14 12:03:30 -0400

    Update documentation

--- src/server/server.js
+++ src/server/server.js
@@ -10,44 +10,44 @@ const {
 
 class Server {
 
+	/**
+	 * Server object has a config and logger properties
+	 * @param config
+	 */
 	constructor (config) {
-		// Validate the config has the required properties
 		this.validate(config);
-		// Store some of the properties
 		this.config = config;
 		this.logger = new Logger(config.logging);
 	}
 
+
 	/**
 	 * @name validate
-	 * @description Validate the config and provide defaults for some values
+	 * @description Validate config and assign default values
 	 * @param {Object} config
 	 */
 	validate (config) {
+
 		// Validate server settings
 		let { server } = config;
-
 		let hasValidServerConfiguration = (
-			// We must have a port
-			server.port &&
-			// If the ssl config is present, it must have a key and cert
-			(!server.ssl || validSSLConfiguration(server.ssl))
+			// Must have server port and either not SSL or SSL with valid key and cert
+			server.port && (!server.ssl || validSSLConfiguration(server.ssl))
 		);
 
 		// Validate logger settings
-		// These are not required, so if something is missing, we will set some sensible defaults
+		// Assign default logging lever "error" if there's no logger setting
 		if (!(config.logging && config.logging.level)) {
-			// Add the default logging level but nothing else
 			config.logging = Object.assign({}, config.logging, { level: 'error' });
 		}
 
 		// Validate auth validator
 		config.auth = loadAuthValidator('auth', config.auth);
 
-		// Grab all the profile keys
+		// Grab all profile keys
 		let profileKeys = Object.keys(config.profiles);
 
-		// Verify that each profile has a service and that it can be loaded
+		// Verify each profile has a service and that it can be loaded
 		let profileHasServicesConfigured = profileKeys.every(profileKey => {
 			return loadProfile(profileKey, config.profiles[profileKey]);
 		});
@@ -55,33 +55,30 @@ class Server {
 		// Throw errors if any of these conditions have failed
 		if (!hasValidServerConfiguration) {
 			throw new Error(
-				'Server configuration is invalid.'
-				+ ' Please review the README.md section on Server Configuration.'
+				'Server configuration is invalid. Please review README.md section on Server Configuration.'
 			);
 		}
 
-		// If we have no profile keys, then they did not setup any profiles for the server
-		// and we can't start
+		// If no profile key, then no profile was set up, so server can't start.
 		if (profileKeys.length === 0) {
 			throw new Error(
-				'No profiles configured. You must configure atleast 1 profile to run this server.'
-				+ ' Please review the README.md section on Configuring Profiles.'
+				'No profiles configured. At least 1 profile must be configured.'
+				+ ' Please review README.md section on Configuring Profiles.'
 			);
 		}
 
-		// Does the profile have services configured. Without services, we cannot get data
-		// so these must be valid and loaded.
+		// If services were not configured, data cannot be loaded.
 		if (!profileHasServicesConfigured) {
 			throw new Error(
 				'No valid profile configurations found.'
-				+ ' Please review the README.md section on Configuring Profiles.'
+				+ ' Please review README.md section on Configuring Profiles.'
 			);
 		}
 
 	}
 
 	start () {
-		this.logger.info('Starting your FHIR Server');
+		this.logger.info('Starting FHIR Server');
 
 		return express.initialize({
 			config: this.config,
@@ -94,11 +91,11 @@ class Server {
 			// Return this instance so the developer can access the logger, config, and express app
 			return this;
 		})
-		.catch(err => {
-			this.logger.error('Fatal error starting the application.', err);
-			// Throw so this can be caught by the adapter
-			throw err;
-		});
+			.catch(err => {
+				this.logger.error('Fatal error starting the application.', err);
+				// Throw so this can be caught by the adapter
+				throw err;
+			});
 
 	}
 
