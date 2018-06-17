292c761bafbac45f616b28b01c8ae3216a885e4e
Rt <37812310+imrts@users.noreply.github.com>
2018-06-17 15:27:33 -0400

    Update response.utils.js read + vread

--- src/server/utils/response.utils.js
+++ src/server/utils/response.utils.js
@@ -1,61 +1,60 @@
-const { resolveFromVersion } = require('./resolve.utils');
+const {resolveFromVersion} = require('./resolve.utils');
 const errors = require('./error.utils');
 
 /**
-* @description When resources are read in the controller functions
-* they all need to respond in a similar manner
-* @function handleSingleReadResponse
-* @param {Express.response} res - Express response object
-* @param {function} next - next function from express middleware
-* @param {string} base - Which spec version is this request coming from
-* @param {T} Resource - Resource class to use for the results
-* @param {object} resource_json - resulting json to be passed in to the class
-*/
+ * @description When resources are read in the controller functions
+ * they all need to respond in a similar manner
+ * @function handleSingleReadResponse
+ * @param {Express.response} res - Express response object
+ * @param {function} next - next function from express middleware
+ * @param {string} base - Which spec version is this request coming from
+ * @param {T} Resource - Resource class to use for the results
+ * @param {object} resource_json - resulting json to be passed in to the class
+ */
 
 let handleSingleReadResponse = (res, next, base, Resource, resource_json, version_id) => {
-	let { version: resource_version } = resource_json;
+	let {version: resource_version} = resource_json;
 
-	if (resource_json) {
-		// If there's resource_version, then compare with requested version_id
-		if ((resource_version) && (version_id !== null)) {
-			if (resource_version === version_id) {
-				//Set ETag header versionId and Last-Modified
+	if (resource_json) { //if there's resource found
+		if (version_id !== null) {
+			if ((resource_version) && (resource_version === version_id)) {
+				//compare version_id vs. resource_version
+				//if matched, send resource; if not matched, send version not found
 				res.set('versionId', version_id);
 				res.set('Last-Modified', resource_json.meta.lastUpdated);
 				res.status(200).json(new Resource(resource_json));
 			} else {
 				next(errors.deleted(`${Resource.__resourceType} version ${version_id} not found.`, base));
 			}
-		}
-		// If there's no resource_version, then just return resource
+		}	//if version_id is null, then return resource regardless of version, just like a regular read
 		res.status(200).json(new Resource(resource_json));
-	} else {
+	} else { //if there's no resource found
 		next(errors.notFound(`${Resource.__resourceType} not found.`, base));
 	}
 };
 
 
 /**
-* @description When resources are read in the controller functions
-* they all need to respond in a similar manner
-* @function handleBundleReadResponse
-* @param {Express.response} res - Express response object
-* @param {string} base - Which spec version is this request coming from
-* @param {T} Resource - Resource class to use for the results
-* @param {Array<object>} resource_json - resulting json to be passed in to the class
-* @param {object} options - Any additional options for configuring the response
-* @param {string} options.resourceUrl - Url for your resource server
-* @param {string} options.resourceType - Type to use in creating the full url for the resource
-* this cannot be inferred all the time because some resources types are not queryable. For example,
-* SmokingStatus is a valid resource type, but /stu3/SmokingStatus/3 can never work, you have to
-* access the resource at /stu3/Observation/3 instead
-* @param {function} options.filter - Filter function to filter the resources out
-* the filter function should expect a resource to be passed in and return a boolean
-*/
+ * @description When resources are read in the controller functions
+ * they all need to respond in a similar manner
+ * @function handleBundleReadResponse
+ * @param {Express.response} res - Express response object
+ * @param {string} base - Which spec version is this request coming from
+ * @param {T} Resource - Resource class to use for the results
+ * @param {Array<object>} resource_json - resulting json to be passed in to the class
+ * @param {object} options - Any additional options for configuring the response
+ * @param {string} options.resourceUrl - Url for your resource server
+ * @param {string} options.resourceType - Type to use in creating the full url for the resource
+ * this cannot be inferred all the time because some resources types are not queryable. For example,
+ * SmokingStatus is a valid resource type, but /stu3/SmokingStatus/3 can never work, you have to
+ * access the resource at /stu3/Observation/3 instead
+ * @param {function} options.filter - Filter function to filter the resources out
+ * the filter function should expect a resource to be passed in and return a boolean
+ */
 let handleBundleReadResponse = (res, base, Resource, resource_json = [], options) => {
 	let Bundle = require(resolveFromVersion(base, 'uscore/Bundle'));
-	let { filter, resourceUrl, resourceType = Resource.__resourceType } = options;
-	let results = new Bundle({ type: 'searchset' });
+	let {filter, resourceUrl, resourceType = Resource.__resourceType} = options;
+	let results = new Bundle({type: 'searchset'});
 	let entries = [];
 
 	if (resource_json) {
@@ -66,7 +65,7 @@ let handleBundleReadResponse = (res, base, Resource, resource_json = [], options
 				// include - This resource is returned because it is referred to from another resource in the search set.
 				// outcome - An OperationOutcome that provides additional information about the processing of a search.
 				entries.push({
-					search: { mode: 'match' },
+					search: {mode: 'match'},
 					resource: new Resource(resource),
 					fullUrl: `${resourceUrl}/${base}/${resourceType}/${resource.id}`
 				});
@@ -81,18 +80,18 @@ let handleBundleReadResponse = (res, base, Resource, resource_json = [], options
 };
 
 /**
-* @description When resources are passed to the create[resource] controller functions
-* they all need to respond in a similar manner with similar headers
-* @function handleCreateResponse
-* @param {Express.response} res - Express response object
-* @param {string} base - Which spec version is this request coming from
-* @param {string} type - type of resource, e.g. Patient or Observation
-* @param {object} results - Results from the operation
-* @param {string} results.id - Id of the updated/created resource
-* @param {string} results.resource_version - Version number of the updated resource
-*/
+ * @description When resources are passed to the create[resource] controller functions
+ * they all need to respond in a similar manner with similar headers
+ * @function handleCreateResponse
+ * @param {Express.response} res - Express response object
+ * @param {string} base - Which spec version is this request coming from
+ * @param {string} type - type of resource, e.g. Patient or Observation
+ * @param {object} results - Results from the operation
+ * @param {string} results.id - Id of the updated/created resource
+ * @param {string} results.resource_version - Version number of the updated resource
+ */
 let handleCreateResponse = (res, base, type, results) => {
-	let { id, resource_version } = results;
+	let {id, resource_version} = results;
 
 	if (resource_version) {
 		res.set('Content-Location', `${base}/${type}/${id}/_history/${resource_version}`);
@@ -103,19 +102,19 @@ let handleCreateResponse = (res, base, type, results) => {
 };
 
 /**
-* @description When resources are passed to the update[resource] controller functions
-* they all need to respond in a similar manner with similar headers
-* @function handleUpdateResponse
-* @param {Express.response} res - Express response object
-* @param {string} base - Which spec version is this request coming from
-* @param {string} type - type of resource, e.g. Patient or Observation
-* @param {object} results - Results from the operation
-* @param {string} results.id - Id of the updated/created resource
-* @param {boolean} results.created - Did the update result in a new resource being created
-* @param {string} results.resource_version - Version number of the updated resource
-*/
+ * @description When resources are passed to the update[resource] controller functions
+ * they all need to respond in a similar manner with similar headers
+ * @function handleUpdateResponse
+ * @param {Express.response} res - Express response object
+ * @param {string} base - Which spec version is this request coming from
+ * @param {string} type - type of resource, e.g. Patient or Observation
+ * @param {object} results - Results from the operation
+ * @param {string} results.id - Id of the updated/created resource
+ * @param {boolean} results.created - Did the update result in a new resource being created
+ * @param {string} results.resource_version - Version number of the updated resource
+ */
 let handleUpdateResponse = (res, base, type, results) => {
-	let { id, created = false, resource_version } = results;
+	let {id, created = false, resource_version} = results;
 	let status = created ? 201 : 200;
 	let date = new Date();
 
@@ -129,21 +128,21 @@ let handleUpdateResponse = (res, base, type, results) => {
 };
 
 /**
-* @description When resources are passed to the delete[resource] controller functions
-* they all need to respond in a similar manner
-* @function handleUpdateResponse
-* @param {Express.response} res - Express response object
-*/
+ * @description When resources are passed to the delete[resource] controller functions
+ * they all need to respond in a similar manner
+ * @function handleUpdateResponse
+ * @param {Express.response} res - Express response object
+ */
 let handleDeleteResponse = (res) => {
 	res.status(204).end();
 };
 
 /**
-* @description When resources are passed to the delete[resource] controller functions
-* they all need to respond in a similar manner with the correct error types
-* @function handleUpdateResponse
-* @param {Express.response} res - Express response object
-*/
+ * @description When resources are passed to the delete[resource] controller functions
+ * they all need to respond in a similar manner with the correct error types
+ * @function handleUpdateResponse
+ * @param {Express.response} res - Express response object
+ */
 let handleDeleteRejection = (res, next, base, err) => {
 	// Make sure the error code is valid
 	if (err.code !== 405 && err.code !== 409) {
