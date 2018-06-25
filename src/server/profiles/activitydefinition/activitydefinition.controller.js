e853fb97023ebab52893e808ec8b332c63eb87ea
Rt <37812310+imrts@users.noreply.github.com>
2018-06-25 14:44:21 -0400

    Add Vread to remaining profiles

--- src/server/profiles/activitydefinition/activitydefinition.controller.js
+++ src/server/profiles/activitydefinition/activitydefinition.controller.js
@@ -3,40 +3,51 @@ const { resolveFromVersion } = require('../../utils/resolve.utils');
 const responseUtils = require('../../utils/response.utils');
 const errors = require('../../utils/error.utils');
 
-module.exports.search = function search ({ profile, logger, config, app }) {
-	let { serviceModule: service } = profile;
+/**
+ * @description Construct a resource with base/uscore path
+ */
+let getResourceConstructor = (base) => {
+	let AllergyIntolerance = require(resolveFromVersion(base, 'base/AllergyIntolerance'));
+	return AllergyIntolerance;
+};
+
+/**
+ * @description Controller to get a resource by history version id
+ */
+module.exports.searchByVersionId = function searchByVersionId({profile, logger, app}) {
+	let {serviceModule: service} = profile;
 
 	return (req, res, next) => {
-		let { base } = req.sanitized_args;
-		// Get a version specific resource
-		let ActivityDefinition = require(resolveFromVersion(base, 'base/ActivityDefinition'));
+		let {base, version_id} = req.sanitized_args;
+		let ActivityDefinition = getResourceConstructor(base);
 
-		return service.search(req.sanitized_args, logger)
+		return service.searchByVersionId(req.sanitized_args, logger)
 			.then((results) =>
-				responseUtils.handleBundleReadResponse( res, base, ActivityDefinition, results, {
-					resourceUrl: config.auth.resourceServer
-				})
+				responseUtils.handleSingleVReadResponse(res, next, base, ActivityDefinition, results, version_id)
 			)
 			.catch((err) => {
 				logger.error(err);
 				next(errors.internal(err.message, base));
 			});
 	};
-
 };
 
 
-module.exports.searchById = function searchById ({ profile, logger, app }) {
-	let { serviceModule: service } = profile;
+/**
+ * @description Controller to search activitydefinition
+ */
+module.exports.search = function search({profile, logger, config, app}) {
+	let {serviceModule: service} = profile;
 
 	return (req, res, next) => {
 		let { base } = req.sanitized_args;
-		// Get a version specific resource
-		let ActivityDefinition = require(resolveFromVersion(base, 'base/ActivityDefinition'));
+		let ActivityDefinition = getResourceConstructor(base);
 
-		return service.searchById(req.sanitized_args, logger)
+		return service.search(req.sanitized_args, logger)
 			.then((results) =>
-				responseUtils.handleSingleReadResponse(res, next, base, ActivityDefinition, results)
+				responseUtils.handleBundleReadResponse(res, base, ActivityDefinition, results, {
+					resourceUrl: config.auth.resourceServer,
+				})
 			)
 			.catch((err) => {
 				logger.error(err);
@@ -46,15 +57,35 @@ module.exports.searchById = function searchById ({ profile, logger, app }) {
 };
 
 /**
- * @description Controller for creating ActivityDefinition
+ * @description Controller to searchById activitydefinition
  */
-module.exports.create = function create ({ profile, logger, app }) {
-	let { serviceModule: service } = profile;
+module.exports.searchById = function searchById({profile, logger, app}) {
+	let {serviceModule: service} = profile;
 
 	return (req, res, next) => {
-		let { base, resource_id, resource_body = {}} = req.sanitized_args;
-		// Get a version specific resource
-		let ActivityDefinition = require(resolveFromVersion(base, 'base/ActivityDefinition'));
+		let { base } = req.sanitized_args;
+		let ActivityDefinition = getResourceConstructor(base);
+
+		return service.searchById(req.sanitized_args, logger)
+			.then((results) => {
+				responseUtils.handleSingleReadResponse(res, next, base, ActivityDefinition, results);
+			})
+			.catch((err) => {
+				logger.error(err);
+				next(errors.internal(err.message, base));
+			});
+	};
+};
+
+/**
+ * @description Controller for creating a activitydefinition
+ */
+module.exports.create = function create({profile, logger, app}) {
+	let {serviceModule: service} = profile;
+
+	return (req, res, next) => {
+		let {base, resource_id, resource_body = {}} = req.sanitized_args;
+		let ActivityDefinition = getResourceConstructor(base);
 		// Validate the resource type before creating it
 		if (ActivityDefinition.__resourceType !== resource_body.resourceType) {
 			return next(errors.invalidParameter(
@@ -62,9 +93,9 @@ module.exports.create = function create ({ profile, logger, app }) {
 				base
 			));
 		}
-		// Create a new resource and pass it to the service
-		let new_resource = new ActivityDefinition(resource_body);
-		let args = { id: resource_id, resource: new_resource };
+		// Create a new activitydefinition resource and pass it to the service
+		let activitydefinition = new ActivityDefinition(resource_body);
+		let args = {id: resource_id, resource: activitydefinition};
 		// Pass any new information to the underlying service
 		return service.create(args, logger)
 			.then((results) =>
@@ -78,15 +109,14 @@ module.exports.create = function create ({ profile, logger, app }) {
 };
 
 /**
- * @description Controller for updating/creating ActivityDefinition. If the ActivityDefinition does not exist, it should be updated
+ * @description Controller for updating/creating a activitydefinition. If the activitydefinition does not exist, it should be updated
  */
-module.exports.update = function update ({ profile, logger, app }) {
-	let { serviceModule: service } = profile;
+module.exports.update = function update({profile, logger, app}) {
+	let {serviceModule: service} = profile;
 
 	return (req, res, next) => {
-		let { base, id, resource_body = {}} = req.sanitized_args;
-		// Get a version specific resource
-		let ActivityDefinition = require(resolveFromVersion(base, 'base/ActivityDefinition'));
+		let {base, id, resource_body = {}} = req.sanitized_args;
+		let ActivityDefinition = getResourceConstructor(base);
 		// Validate the resource type before creating it
 		if (ActivityDefinition.__resourceType !== resource_body.resourceType) {
 			return next(errors.invalidParameter(
@@ -94,9 +124,9 @@ module.exports.update = function update ({ profile, logger, app }) {
 				base
 			));
 		}
-		// Create a new resource and pass it to the service
-		let new_resource = new ActivityDefinition(resource_body);
-		let args = { id, resource: new_resource };
+		// Create a new activitydefinition resource and pass it to the service
+		let activitydefinition = new ActivityDefinition(resource_body);
+		let args = {id, resource: activitydefinition};
 		// Pass any new information to the underlying service
 		return service.update(args, logger)
 			.then((results) =>
@@ -110,10 +140,10 @@ module.exports.update = function update ({ profile, logger, app }) {
 };
 
 /**
- * @description Controller for deleting an ActivityDefinition.
+ * @description Controller for deleting an activitydefinition resource.
  */
-module.exports.remove = function remove ({ profile, logger, app }) {
-	let { serviceModule: service } = profile;
+module.exports.remove = function remove({profile, logger, app}) {
+	let {serviceModule: service} = profile;
 
 	return (req, res, next) => {
 		let { base } = req.sanitized_args;
