db7e309f5a2cb19e756c92b3175c494d54c0d930
Nathan Hall <nathan.mh@gmail.com>
2018-12-19 12:35:50 -0500

    Add search by id (#117)

--- src/server/operations/operations.controller.js
+++ src/server/operations/operations.controller.js
@@ -31,10 +31,8 @@ module.exports.operationsPost = function operationsPost({profile, logger, funcNa
 module.exports.operationsGet = function operationsGet({profile, logger, funcName}) {
   let {serviceModule: service} = profile;
   return (req, res, next) => {
-    let { base_version, resource_id} = req.sanitized_args;
-    let resource_body = req.body;
-    let args = {id: resource_id, base_version, resource: resource_body};
-    service[funcName](args, req.contexts, logger)
+    let { base_version} = req.sanitized_args;
+    service[funcName](req.sanitized_args, req.contexts, logger)
       .then(results => {
         responseUtils.handleOperationResponse(res, next, base_version, results);
       })
