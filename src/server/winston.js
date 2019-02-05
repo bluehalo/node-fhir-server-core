280efa9a754de914fa7e0e700249305e5e09561c
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2019-02-05 10:51:14 -0500

    fix: issue with loggers getting initialized before server resulting in no transports being created

--- src/server/winston.js
+++ src/server/winston.js
@@ -11,37 +11,37 @@ const { Container, transports } = require('winston');
  let container = new Container();
 
 /**
- * @function add
- * @description Expose the winston logger to make it easy to add more loggers
- * for the end user
- */
-module.exports.add = container.add;
-
-/**
  * @function get
  * @description Retrieve a logger by name, can be one the user created or not
  * @param {String} name - Name of the logger
+ * @param {Object} options - Options for the logger, this is also an alias for
+ * adding a logger
  * @return {winston.logger}
  */
-module.exports.get = (name = 'default') => container.get(name);
+module.exports.get = (name = 'default', options) => container.get(name, options);
 
 /**
  * @function initialize
  * @description Initialize a default console logger
  */
 module.exports.initialize = (config = {}) => {
-	// If we already have a logger by the provided default name remove it
-	// if (container.has('default')) {
-	// 	container.close('default');
-	// }
-
-	container.add('default', {
-		transports: [
-			new transports.Console({
-				level: config.level,
-				timestamp: true,
-				colorize: true,
-			}),
-		],
+	let transport = new transports.Console({
+		level: config.level,
+		timestamp: true,
+		colorize: true,
 	});
+	// If we already have a logger by the provided default name, make sure it
+	// has a console transport added. This can happen when someone accesses the
+	// logger before calling initialize
+	if (container.has('default')) {
+		let logger = container.get('default');
+		// Only add the console logger if none is present, technically
+		if (logger.transports.length === 0) {
+			logger.add(transport);
+		}
+	} else {
+		container.add('default', {
+			transports: [transport],
+		});
+	}
 };
