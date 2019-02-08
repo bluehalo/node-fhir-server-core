df3666be19d9101f601cd6a5207a39b147715075
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2019-02-08 09:31:56 -0500

    chore: Added tests for resolve and resolveFromVersion utils

--- src/server/utils/resolve.utils.js
+++ src/server/utils/resolve.utils.js
@@ -1,23 +1,13 @@
 const path = require('path');
-const fs = require('fs');
 
 let base = path.join(__dirname, '../..');
 
-let resolveFromVersion = (version, relative_path) => {
-	let fileIndex;
-	let files;
-	if (relative_path && typeof relative_path === 'string') {
-		files = fs.readdirSync(path.join(__dirname + `/../standards/${version}`)).map(filename => {
-			return filename;
-		});
-
-		const filesLowerCase = files.map(elm => elm.split('.')[0].toLowerCase());
-		fileIndex = filesLowerCase.indexOf(relative_path.toLowerCase());
-	}
-	return path.join(base, '/server/standards/', version, files[fileIndex] ? files[fileIndex] : relative_path);
+let resolveFromVersion = (version = '4_0_0', relative_path = '') => {
+	let lowercaseKey = relative_path.toLowerCase();
+	return path.join(base, '/server/resources/', version, 'schemas', lowercaseKey);
 };
 
-let resolve = relative_path => {
+let resolve = (relative_path = '') => {
 	return path.join(base, relative_path);
 };
 
---
+++ src/server/utils/resolve.utils.test.js
@@ -0,0 +1,37 @@
+const { resolveFromVersion, resolve } = require('./resolve.utils');
+const path = require('path');
+
+describe('Resolve Utils Test', () => {
+	describe('resolve', () => {
+		test('should resolve to `src` as the base when no relative path is provided', () => {
+			let base = path.basename(resolve());
+			expect(base).toEqual('src');
+		});
+
+		test('should resolve to a path relative to the base', () => {
+			let relative_path = 'scooby/doo';
+			let diff = path.relative(resolve(''), resolve(relative_path));
+			expect(diff).toEqual(relative_path);
+		});
+	});
+
+	describe('resolveFromVersion', () => {
+		test('should resolve to `src/resources/stu3` when no arguments are provided', () => {
+			let base = resolveFromVersion();
+			let project_base = base.substr(base.indexOf('src'));
+			expect(project_base).toEqual('src/server/resources/4_0_0/schemas');
+		});
+
+		test('should resolve to `src/resources/r4` when no relatve path is provided and version is r4', () => {
+			let base = resolveFromVersion('r4');
+			let project_base = base.substr(base.indexOf('src'));
+			expect(project_base).toEqual('src/server/resources/r4/schemas');
+		});
+
+		test('should resolve a path relative to the version base', () => {
+			let base = resolveFromVersion('3_0_1', 'scooby/doo');
+			let project_base = base.substr(base.indexOf('src'));
+			expect(project_base).toEqual('src/server/resources/3_0_1/schemas/scooby/doo');
+		});
+	});
+});
