fa47dadc6ab6249826ae99604dcc313fb47f5e3d
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2019-05-13 15:17:36 -0400

    chore: updated operations controller signature to match other controllers

--- src/server/operations/operations.controller.js
+++ src/server/operations/operations.controller.js
@@ -11,7 +11,7 @@ module.exports.operationsPost = function operationsPost({ profile, name, logger:
 		let resource_body = req.body;
 		let args = { id: resource_id, base_version, resource: resource_body };
 
-		service[name](args, req.contexts, deprecatedLogger)
+		service[name](args, { req }, deprecatedLogger)
 			.then(results => handler.read(req, res, results))
 			.catch(next);
 	};
@@ -23,7 +23,7 @@ module.exports.operationsPost = function operationsPost({ profile, name, logger:
 module.exports.operationsGet = function operationsGet({ profile, name, logger: deprecatedLogger }) {
 	let { serviceModule: service } = profile;
 	return (req, res, next) => {
-		service[name](req.sanitized_args, req.contexts, deprecatedLogger)
+		service[name](req.sanitized_args, { req }, deprecatedLogger)
 			.then(results => handler.read(req, res, results))
 			.catch(next);
 	};
