cc677b12e47920a6a24e0c48fc974824b08d96ff
Pétur Þór Valdimarsson <petur@omnivore.se>
2019-06-13 13:50:00 +0200

    Fixed issue where configuration passed to authenticationMiddleware could not be accessed properly Added test for issue.

--- src/server/router.test.js
+++ src/server/router.test.js
@@ -3,6 +3,9 @@ jest.mock('./middleware/version-validation.middleware.js');
 const router = require('./router.js');
 const path = require('path');
 
+const passport = require('passport');
+jest.mock('passport');
+
 let app, config;
 
 let mockServiceModule = {
@@ -144,5 +147,28 @@ describe('Router Tests', () => {
 		expect(app.post.mock.calls).toHaveLength(2);
 		expect(app.post.mock.calls[0][0]).toBe('/:base_version/patient/([$])aggregate-results');
 		expect(app.post.mock.calls[1][0]).toBe('/:base_version/patient/_search');
+	});
+
+	test('config should not be wrapped in another object before being passed to authentication middleware', () => {
+		let originalEnv = process.env.NODE_ENV;
+		process.env.NODE_ENV = 'development';
+
+		config.auth = {
+			strategy: {
+				name: 'test',
+				useSession: true,
+			}
+		};
+		config.profiles.patient.serviceModule = mockServiceModule;
+
+		try {
+			router.setRoutes({ app, config });
+		} catch (e) {
+			// Failed
+		}
+
+		expect(passport.authenticate.mock.calls.length).toBe(2);
+
+		process.env.NODE_ENV = originalEnv;
 	});
 });
